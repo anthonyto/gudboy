53adae4a56bff2cdc5ec1851a877269beb1fb708
Anthony To <to.anthony93@gmail.com>
2016-03-20 21:24:24 +1100

    Remove exp from jwt

--- lib/auth_token.rb
+++ lib/auth_token.rb
@@ -1,12 +1,11 @@
 require 'jwt'
 
 class AuthToken
-  def self.encode(payload, exp=24.hours.from_now)
-    payload[:exp] = exp
+  def self.encode(payload)
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
   
-  def self.decode(auth_token)
+  def self.decode(token)
     JWT.decode(token, Rails.application.secrets.secret_key_base)
     DecodedAuthToken.new(payload)
   rescue
