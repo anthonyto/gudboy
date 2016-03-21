fc9bcba512f1433bc8e73ed8acacd516978b72fd
Anthony To <to.anthony93@gmail.com>
2016-03-22 10:10:34 +1100

    Add user is to categories table

---
+++ db/migrate/20160321220900_add_user_id_to_categories.rb
@@ -0,0 +1,5 @@
+tclass AddUserIdToCategories < ActiveRecord::Migration
+  def change
+    add_column :categories, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321220726) do
+ActiveRecord::Schema.define(version: 20160321220900) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string  "name",    limit: 255
+    t.integer "user_id", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
