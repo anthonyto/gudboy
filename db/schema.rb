abc1f91faf4503e68bf9d3478cefc1324aafcd77
Anthony To <to.anthony93@gmail.com>
2016-03-22 09:07:58 +1100

    Create categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317043326) do
+ActiveRecord::Schema.define(version: 20160321220726) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", limit: 255
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name", limit: 255
