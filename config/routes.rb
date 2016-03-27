b39e9d66765dfa21463c4121c8a2fde2ed629c53
Anthony To <to.anthony93@gmail.com>
2016-03-27 13:03:16 +1100

    Add defaults json to all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   post 'auth' => 'auth#authenticate'
   resource :user, only: [:index, :show, :create, :update], defaults: { format: :json }
-  resources :categories, only: [:index, :show, :create, :update] do 
-    resources :transactions, only: [:index, :show, :create, :update, :destroy]
+  
+  resources :categories, only: [:index, :show, :create, :update], defaults: { format: :json } do 
+    resources :transactions, only: [:index, :show, :create, :update, :destroy], defaults: { format: :json }
   end
   
   root to: 'auth#authenticate'
